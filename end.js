04aa4f3f7bda8025ca117f8ea0ff51073086f2ee
Mahak Juriani <juriani.mahak@gmail.com>
2024-05-21 23:58:58 +0530

    git commit -m "first commit"

---
+++ end.js
@@ -0,0 +1,8 @@
+
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+
+const score = {
+    score: mostRecentScore,
+};
